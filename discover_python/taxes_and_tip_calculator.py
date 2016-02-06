5520804e9e638a900e7ff98b7cce0218f42baf44
Alexandro de Oliveira <23@p3-1.hbtn.io>
2016-02-06 13:45:52 -0800

    Discovering Python. Task 0

---
+++ discover_python/taxes_and_tip_calculator.py
@@ -0,0 +1,25 @@
+"""
+This is the taxes_and_tip_calculator.py, the file for task of the 
+"Discover Python" by Alexandro de Oliveira
+"""
+ 
+print "Welcome to the taxes and tip calculator!"
+price = input("What is the price before tax? ")
+tax = input("What are the taxes? (in %) ")
+tip = input("What do you want to tip? (in %) ")
+
+#Using just one function to do all the job
+#This first function just calculates the percentage
+def perc(n):
+	return float(n)/100
+
+#This second function calls the first in the second and third lines, adding the price to it and returns the total price in the last line
+def taxntips(p,tax,tip):
+	p += p * perc(tax)
+	p += p * perc(tip)
+	return p 
+
+#Here I'm using the function taxntips to calculate the total
+total = taxntips(price, tax, tip)
+#Formating the output to meet requirements
+print "The price you need to pay is: $%.6f." % total
