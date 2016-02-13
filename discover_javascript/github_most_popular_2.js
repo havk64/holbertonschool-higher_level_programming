89ac35a586fdc166db5d4cce5f686885dfc6d61e
Alexandro de Oliveira <havk.fs@gmail.com>
2016-02-13 12:12:54 -0800

    Task 1 solved

---
+++ discover_javascript/github_most_popular_2.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+var options = {
+	hostname: 'api.github.com',
+	path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
+	headers: {
+	'User-Agent': 'Holberton_School',
+	'Authorization': 'token 8ac478d797eecd39a3f8198d432dd772882f1c41'
+  }
+}
+
+function streamToString(stream, cb) {
+  const chunks = [];
+  stream.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+  stream.on('end', () => {
+    cb(chunks.join(''));
+  });
+}
+var req = https.request(options, function(res) {
+	streamToString(res, function(data){
+	console.log(typeof data);
+	console.log(data);
+})
+
+	//res.on('data', function(d) {
+	//process.stdout.write(d);
+	//});
+});
+req.end();
+
+req.on('error', function(e) {
+	console.log(e);
+});
+
