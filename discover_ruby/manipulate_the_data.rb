aa64b415c26cd54c537ec1e269d7f74f03fb16c3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-21 02:09:16 -0300

    Task 2 done

---
+++ discover_ruby/manipulate_the_data.rb
@@ -0,0 +1,15 @@
+require 'HTTPClient'
+require "rubygems"
+require "json"
+
+extheaders = {
+  'User-Agent' => 'Holberton_School',
+  'Authorization' => 'token 6a54def2525aa32b003337b31487e321d6a2bb59'
+}
+
+search = 'https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc'
+
+http = HTTPClient.new
+res = http.get_content(search, extheaders)
+string = JSON.parse(res)
+puts string["items"].map { |item| item["full_name"]}.join("\n")
