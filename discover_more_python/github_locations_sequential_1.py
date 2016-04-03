05554bc1e1c3af8574f7423dbddbfb9c2a16b4a6
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-03 11:24:31 -0300

    Optimizing the original code dismissing unnecessary function

--- discover_more_python/github_locations_sequential_1.py
+++ discover_more_python/github_locations_sequential_1.py
@@ -1,31 +1,5 @@
 import urllib2
 import json
-import sys
-
-"""This function will convert any decoded JSON object from using unicode strings \
-to UTF-8-encoded byte strings"""
-def json_loads_byteified(json_text):
-    return _byteify(
-        json.loads(json_text, object_hook=_byteify),
-        ignore_dicts=True
-    )
-
-def _byteify(data, ignore_dicts = False):
-    # if this is a unicode string, return its string representation
-    if isinstance(data, unicode):
-        return data.encode('utf-8')
-    # if this is a list of values, return list of byteified values
-    if isinstance(data, list):
-        return [ _byteify(item, ignore_dicts=True) for item in data ]
-    # if this is a dictionary, return dictionary of byteified keys and values
-    # but only if we haven't already byteified it
-    if isinstance(data, dict) and not ignore_dicts:
-        return {
-            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
-            for key, value in data.iteritems()
-        }
-    # if it's anything else, return it in its original form
-    return data
 
 request_headers = {
         'User-Agent': 'Holberton_School',
@@ -44,9 +18,8 @@ def get_location(user):
     user_content = urllib2.urlopen(req).read()
     content = json.loads(user_content)
     user_obj = {'full_name': user['full_name'],'location':content['location']}
-    return json.dumps(user_obj)
+    return user_obj
 python_masters = []
 for user in json_data['items']:
-    python_masters.append(json_loads_byteified(get_location(user)))
-print python_masters
-
+    python_masters.append(get_location(user))
+print json.dumps(python_masters)
