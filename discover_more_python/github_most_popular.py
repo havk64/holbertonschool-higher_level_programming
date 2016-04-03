5b507fd25cd26039190e2128a5dbd8b29de10b4d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-02 21:08:33 -0300

    Task 0 completed

---
+++ discover_more_python/github_most_popular.py
@@ -0,0 +1,11 @@
+import urllib2
+
+request_headers = {
+        'User-Agent': 'Holberton_School',
+        'Authorization': 'token 6a54def2525aa32b003337b31487e321d6a2bb59'
+    }
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+
+request = urllib2.Request(url, headers=request_headers)
+contents = urllib2.urlopen(request).read()
+print contents
