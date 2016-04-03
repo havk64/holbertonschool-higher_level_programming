7d88b12ea0caa2199b4c5d8d98f88720090ee1d9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-03 17:23:26 -0300

    Catching exceptions

--- discover_more_python/github_locations_sequential_1.py
+++ discover_more_python/github_locations_sequential_1.py
@@ -1,15 +1,43 @@
+import urllib
 import urllib2
+import urlparse
 import json
+import sys
 
 request_headers = {
         'User-Agent': 'Holberton_School',
         'Authorization': 'token 6a54def2525aa32b003337b31487e321d6a2bb59'
     }
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+url = 'https://api.github.com'
+path = '/search/repositories'
+query = {
+    'q': 'language:python',
+    'sort':'stars',
+    'order':'desc'
+}
 
-request = urllib2.Request(url, headers=request_headers)
-contents = urllib2.urlopen(request).read()
-json_data = json.loads(contents)
+def http_req(path, query):
+    purl = list(urlparse.urlparse(url))
+    purl[2] = path
+    purl[4] = urllib.urlencode(query)
+    purl = urlparse.urlunparse(purl)
+    request = urllib2.Request(purl,headers=request_headers)
+
+    try:
+        contents = urllib2.urlopen(request).read()
+        return json.loads(contents)
+    except urllib2.HTTPError as error:
+        print "Something went wront"
+        print "The main reason is: ", str(error.reason)
+        print error
+        sys.exit(0)
+    except urllib2.URLError as error:
+        print "Something went wrong!"
+        print "The main reason is: ", str(error.reason)
+        print error
+        sys.exit(0)
+        
+json_data = http_req(path,query)
 
 #Function used to get the location of each user and concatenate with its full name.
 def get_location(user):
