7d8ef1482d8c8ef331213a708ce65a74ec76abde
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-03 22:21:49 -0300

    Adding exception handler, calling just methods needed

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,11 +1,18 @@
-import urllib2
+from urllib2 import Request, urlopen, HTTPError, URLError
 
 request_headers = {
         'User-Agent': 'Holberton_School',
         'Authorization': 'token 6a54def2525aa32b003337b31487e321d6a2bb59'
     }
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+data = {}
+request = Request(url, headers=request_headers)
 
-request = urllib2.Request(url, headers=request_headers)
-contents = urllib2.urlopen(request).read()
-print contents
+try:
+    print urlopen(request).read()
+except HTTPError as error:
+    print "Something went wrong!"
+    print error
+except URLError as error:
+    print "Something went wrong!"
+    print error
