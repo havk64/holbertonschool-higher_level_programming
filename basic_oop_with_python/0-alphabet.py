fa9f424e82ea1a270109371573aee663eb22559f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-17 14:37:01 -0300

    Adding comments

--- basic_oop_with_python/0-alphabet.py
+++ basic_oop_with_python/0-alphabet.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import sys
+
+'''
+    Function to print to stdout each sequence from a to b.
+'''
 def alphabet(a,b):
     for i in xrange(ord(a),ord(b)+1):
         sys.stdout.write(chr(i))
 
+#Calling the function:
 alphabet('a', 'z')
