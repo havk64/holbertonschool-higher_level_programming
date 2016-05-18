a67684b6d470f8ec1024765080701547355a4432
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-18 12:48:04 -0300

    First commit task 4

--- basic_oop_with_python/square.py
+++ basic_oop_with_python/square.py
@@ -9,14 +9,23 @@ class Square:
     def get_color():
         return self.__color
 
-    def set_color(color):
+    def set_color(self, color):
         self.__color = color
 
     def get_center():
         return self.__color
 
-    def set_center(value):
+    def set_center(self, value):
         self.__center = value
 
     def area(self):
         return self.__side_length * self.__side_length
+
+
+
+    def __call__(n):
+        return "Not printing yet"
+
+
+    def __str__(self):
+        return "Printing Square next..."
