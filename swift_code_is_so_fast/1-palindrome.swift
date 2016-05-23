d800a5bf30cf68a1607259dce84ecf77efc7d58c
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-23 17:55:36 -0300

    Task 1 completed

---
+++ swift_code_is_so_fast/1-palindrome.swift
@@ -0,0 +1,13 @@
+// ===---------------------------------------===//
+// Project "Swift" by Paulin Laroche - Task 1
+// by Alexandro de Oliveira, for Holberton School.
+// ===---------------------------------------===//
+
+/**
+ * @discussion: Function is_palindrome checks if a string is a palindrome.
+ * @params: Accepts a string as argument.
+ * @returns: returns a boolean value if the string is a palindrome or not.
+ */
+func is_palindrome(s: String) -> Bool {
+  return String(s.characters.reverse()) == s
+}
