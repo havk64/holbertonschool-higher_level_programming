0a31bbc2609acb11caea2a6400267879b5e9ffc1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-23 04:23:02 -0300

    Task 3 completed

--- oop_can_manage_your_family/family.py
+++ oop_can_manage_your_family/family.py
@@ -1,4 +1,7 @@
 from datetime import date
+from os.path import isfile
+from json import dumps
+from json import load
 
 """Class definition"""
 class Person():
@@ -61,12 +64,109 @@ class Person():
     def is_male(self):
         return self.__genre == "Male"
 
+    """Checks the actual age"""
     def age(self):
         birth = date(self.__date_of_birth[2], self.__date_of_birth[0], self.__date_of_birth[1])
         day = date(2016, 5, 20)
         return ((day - birth).days/365)
 
+    """To compare"""
     def __cmp__(self, right):
         return cmp(self.age(), right.age())
 
-    
+    def json(self):
+        return {
+        'id': self.get_id(),
+        'eyes_color': self.get_eyes_color(),
+        'genre': self.get_genre(),
+        'date_of_birth': self.get_date_of_birth(),
+        'first_name': self.get_first_name(),
+        'last_name': self.last_name,
+        }
+
+    def load_from_json(self, json):
+        if type(json) is not dict:
+            raise Exception("json is not valid")
+
+        self.__id = json['id']
+        self.__first_name = json['first_name']
+        self.__date_of_birth = json['date_of_birth']
+        self.__genre = json['genre']
+        self.__eyes_color = json['eyes_color']
+        self.last_name = json['last_name']
+
+
+
+class Baby(Person):
+    def can_run(self):
+        return False
+    def need_help(self):
+        return True
+    def is_young(self):
+        return True
+    def can_vote(self):
+        return False
+
+class Teenager(Person):
+    def can_run(self):
+        return True
+    def need_help(self):
+        return False
+    def is_young(self):
+        return True
+    def can_vote(self):
+        return False
+
+class Adult(Person):
+    def can_run(self):
+        return True
+    def need_help(self):
+        return False
+    def is_young(self):
+        return False
+    def can_vote(self):
+        return True
+
+
+class Senior(Person):
+    def can_run(self):
+        return False
+    def need_help(self):
+        return True
+    def is_young(self):
+        return False
+    def can_vote(self):
+        return True
+
+#
+def save_to_file(list, filename):
+    if not isfile(filename) or type(filename) is not str:
+        raise Exception("filename is not valid or doesn't exist")
+
+    with open(filename, 'w') as f:
+        output = []
+        for item in list:
+            data = item.json()
+            data['type'] =  item.__class__.__name__
+            output.append(data)
+        f.write(dumps(output))
+
+def load_from_file(filename):
+    if not isfile(filename) or type(filename) is not str:
+        raise Exception("filename is not valid or doesn't exist")
+
+    with open(filename) as f:
+        output = []
+        data = load(f)
+        for item in data:
+            if item['type'] == 'Baby':
+                n = Baby(0, 'test', [1, 1, 1900], "Male", "Blue")
+            if item['type'] == 'Teenager':
+                n = Teenager(0,'test', [1, 1, 1900], "Male", "Blue")
+            if item['type'] == 'Adult':
+                n = Adult(0, 'test', [1, 1, 1900], "Male", "Blue")
+            if item ['type'] == 'Senior':
+                n = Senior(0, 'test', [1, 1, 1900], "Male", "Blue")
+            n.load_from_json(item)
+            output.append(n)
+        return output
