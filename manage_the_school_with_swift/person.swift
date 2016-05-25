b7285e0d7a0059b08cd908fb546a076e7dad6457
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-25 02:49:01 -0300

    Task 1 completed

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -1,21 +1,47 @@
 // ===----------------------------------------------------------------===//
-//   Project "Manage the school with Swift" by Paulin Laroche - Task 0
+//      Project "Manage the school with Swift" by Paulin Laroche
 //          by Alexandro de Oliveira, for Holberton School.
 // ===----------------------------------------------------------------===//
 
 /**
- * @discussion Declaring the class Person.
- */
+* @discussion Declaring the class Person.
+*/
 class Person {
-  var first_name = "" // Public attributes.
-  var last_name = ""  
-  var age = 0
-  init(first_name: String, last_name: String, age: Int) { // The initializer.
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-  }
-  func fullName() -> String { // A public method.
-    return self.first_name + " " + self.last_name
-  }
+    var first_name = "" // Public attributes.
+    var last_name = ""
+    var age = 0
+    init(first_name: String, last_name: String, age: Int) { // The initializer.
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    }
+    func fullName() -> String { // A public method.
+        return self.first_name + " " + self.last_name
+    }
+}
+
+/**
+* @discussion Declaring the class Mentor.
+*/
+class Mentor: Person, Classify {
+
+    func isStudent() -> Bool { // Declaring isStudent() to meet protocol requirements.
+        return false
+    }
+}
+
+/**
+* @discussion Declaring the class Student.
+*/
+class Student: Person, Classify {
+    func isStudent() -> Bool { // Declaring isStudent to meet protocol requirements.
+        return true
+    }
+}
+
+/**
+* @discussion Declaring the protocol Classify.
+*/
+protocol Classify {
+    func isStudent() -> Bool
 }
