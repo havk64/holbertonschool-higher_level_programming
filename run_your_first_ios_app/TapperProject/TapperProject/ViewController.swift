50f9519198985b67c2e7ffa2fa294a28d1378724
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-29 00:47:41 -0300

    Task 3 completed

--- run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
+++ run_your_first_ios_app/TapperProject/TapperProject/ViewController.swift
@@ -16,11 +16,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var label_taps: UILabel!
     @IBOutlet weak var button_coin: UIButton!
     
+    var taps_done: Int = 0
+    var taps_requested: Int = 0
+    
     @IBAction func clickPlayButton(sender: AnyObject){
         if let taps = Int(self.textfield_number!.text!) {
             if taps > 0 {
                 print("Let's do \(taps) taps")
             }
+            self.taps_requested = taps
+            initGame()
         }
     }
     
@@ -28,9 +33,33 @@ class ViewController: UIViewController {
         print("Tap!")
         self.taps_done += 1
         self.label_taps.text = "\(taps_done) Taps"
+        if self.taps_done >= self.taps_requested{
+            resetGame()
+        }
     }
-    var taps_done: Int = 0
-    var taps_requested: Int = 0
+    
+    func initGame() {
+        self.image_tapper.hidden = true
+        self.button_play.hidden = true
+        self.textfield_number.hidden = true
+        self.label_taps.hidden = false
+        self.button_coin.hidden = false
+        self.taps_done = 0
+        self.label_taps.text = "0 Taps"
+    }
+    
+    func resetGame() {
+        self.label_taps.hidden = true
+        self.button_coin.hidden = true
+        self.image_tapper.hidden = false
+        self.button_play.hidden = false
+        self.textfield_number.hidden = false
+        self.taps_requested = 0
+        
+    }
+    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
