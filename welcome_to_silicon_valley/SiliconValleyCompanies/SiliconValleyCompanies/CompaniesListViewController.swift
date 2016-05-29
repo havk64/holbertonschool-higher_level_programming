b960955e98533ab2f06dd1c9b8ebe9d19599b4be
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-29 20:48:10 -0300

    Task 1 completed

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/CompaniesListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CompaniesListViewController.swift
+//  SiliconValleyCompanies
+//
+//  Created by Alexandro on 5/29/16.
+//  Copyright © 2016 Holberton School. All rights reserved.
+//
+
+import Cocoa
+
+class CompaniesListViewController: UITableView {
+
+}
