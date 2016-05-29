1c03a739fbbde0fadcef6ecac28c6a4a71e44018
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-29 19:53:26 -0300

    Task 0 completed

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiliconValleyCompanies
+//
+//  Created by Alexandro on 5/29/16.
+//  Copyright © 2016 Holberton School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
