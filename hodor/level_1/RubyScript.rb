b90f9dada993762fd9e939f4ce19717c02ad1b13
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-30 05:04:29 -0300

    Adding comments, description. Hodor 1 - Ruby script

--- hodor/level_1/RubyScript.rb
+++ hodor/level_1/RubyScript.rb
@@ -3,21 +3,33 @@
 require 'uri'
 require 'net/http'
 
-uri = URI.parse("http://173.246.108.142/level1.php")
-cookie = "6b695713db9b371a855adf1e229264c681f2e48"
-http = Net::HTTP.new(uri.host, uri.port)
+=begin <= Ruby multiline comment
+===---Description---------------------------------------------------------------===
+# Solution for the Hodor Project, Level 1 by Julien Barbier.
+# Using Ruby Language.
+# by Alexandro de Oliveira, for Holberton School
+===-----------------------------------------------------------------------------===
+=end
 
+# Parsing the URL:
+uri = URI.parse("http://173.246.108.142/level1.php")
+# Setting the cookie:
+cookie = "6b695713db9b371a855adf1e229264c681f2e48"
+# Setting the http request:
+http = Net::HTTP.new(uri.host, uri.port)
+# Setting the http request headers:
 headers = {
     'User-Agent' => 'havk64 - Ruby Requests',
     'Content-Type' => 'application/x-www-form-urlencoded',
     'Cookie' => 'HoldTheDoor=' << cookie
 }
-puts headers
+# Setting POST request parameters:
 params = URI.encode_www_form({
     'id' => "23",
     'holdthedoor' => "submit",
     'key' => cookie
 })
-
+# Making the POST request:
 resp, data = http.post(uri.path, params, headers)
+# Optionally printing the response object hash:
 puts resp.to_hash
