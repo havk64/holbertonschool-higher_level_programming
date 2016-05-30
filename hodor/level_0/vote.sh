4fd1b6a88fcb8e2c811799b9709cd4a7323da0b0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-30 02:42:54 -0300

    Improving comments, description

--- hodor/level_0/vote.sh
+++ hodor/level_0/vote.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-### Solution in Shell Script for Hodor Project by Julien Barbier - Holberton School
-### Using curl command and its default 'keep-alive' connection.
+# ===-----------------------------------------------------------------------------===
+# Solution in Shell Script for Hodor Project by Julien Barbier - Holberton School
+# Using curl command and its default 'keep-alive' connection.
+# If you want to see more details about each request replace the "s" option with "v"
+# right after the 'curl' command.	
+# ===-----------------------------------------------------------------------------===
 
 URL=http://173.246.108.142/level0.php
-HEADERS='User-Agent: havk64 - Curl requests'
+AGENT='havk64 - Curl requests'
 ID='id=23'
 COUNT=0
 VOTES=1024
@@ -12,7 +16,7 @@ VOTES=1024
 ### Voting 1024 times:
 while [ $COUNT -lt $VOTES ]
 do
-    curl -s -d "$ID&holdthedoor=submit" -H "$HEADERS"  $URL -o /dev/null
+    curl -s -d "$ID&holdthedoor=submit" -A "$AGENT"  $URL -o /dev/null
     echo "Vote: "$COUNT
     ((COUNT++))
 done
