dc531661cf4e98918c607f06d5d3789cae1bba2a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-31 21:01:49 -0300

    Fixing strange behavior when connecting

--- hodor/level_0/hodor0.c
+++ hodor/level_0/hodor0.c
@@ -1,9 +1,11 @@
+
 /***
- * ===---Description------------------------------------------------------------===
- *  Solution for the Hodor Project, Level 0 by Julien Barbier.
- *  Using C Language!    BOOOOMMMMMM ! ! ! \o/ \o/ \o/
- *  by Alexandro de Oliveira, for Holberton School
- * ===--------------------------------------------------------------------------===
+ * ===---Description---------------------------------------------------------------===
+ *   Solution for the Hodor Project, Level 1 by Julien Barbier.
+ *   Using C Language!    BOOOOMMMMMM ! ! ! \o/ \o/ \o/
+ *
+ *   by Alexandro de Oliveira, for Holberton School
+ * ===-----------------------------------------------------------------------------===
  */
 
 #include <stdio.h>      /* printf, perror */
@@ -13,7 +15,7 @@
 #include <sys/socket.h> /* socket, connect */
 #include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
 #include <netdb.h>      /* struct hostent, gethostbyname */
-
+void vote();
 
 void error(const char *msg)
 {
@@ -40,31 +42,31 @@ void vote()
     /* Setting the http port, hostname and message */
     int portno      =  80;
     char *host      =  "173.246.108.142";
-    char *message   =  "POST /level0.php HTTP/1.0\r\nContent-Type: application/x-www-form-urlencoded\r\nUser-Agent:C language POST Requests - Alexandro de Oliveira\r\nContent-Length:24\r\n\r\nid=23&holdthedoor=submit\r\n";
+    char *message   =  "POST /level0.php HTTP/1.1\r\nUser-Agent:havk64 - C language requests\r\nCookie:HoldTheDoor=990aede99b2115451d612b8c50ae6332a8026b62\r\nContent-Length:69\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nid=23&holdthedoor=submit&key=990aede99b2115451d612b8c50ae6332a8026b62\r\n";
 
     struct hostent *server;
     struct sockaddr_in serv_addr;
     int sockfd, bytes, sent, received, total;
     char response[4096];
 
-    /* create the socket */
+    /* Creating the Socket */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) error("ERROR opening socket");
 
-    /* lookup the ip address */
+    /* Lookup the IP address */
     server = gethostbyname(host);
     if (server == NULL) error("ERROR, no such host");
 
-    /* fill in the structure */
+    /* Filling in the structure */
     memset(&serv_addr,0,sizeof(serv_addr));     /* Filling the sockarrd_in struct with '0's */
     serv_addr.sin_family = AF_INET;             /* Internet Protocol(IP) v4 addresses */
     serv_addr.sin_port = htons(portno);         /* TCP/IP port */
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length); /* Now we have the host address in the struct */
+    memcpy(&serv_addr.sin_addr.s_addr,server -> h_addr,server -> h_length); /* Now we have the host address in the struct */
 
-    /* connect the socket */
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+    /* Connecting the Socket */
+    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
-    /* send the request */
+    /* Sending the Request */
     total = strlen(message);
     sent = 0;
     do {
@@ -76,9 +78,9 @@ void vote()
         sent+=bytes;
     } while (sent < total);
 
-    /* receive the response */
-    memset(response,0,sizeof(response));
-    total = sizeof(response)-1;
+    /* Receiving the response */
+    memset(response, 0, sizeof(response));
+    total = sizeof(response) - 1;
     received = 0;
     do {
         bytes = read(sockfd,response+received,total-received);
@@ -92,8 +94,8 @@ void vote()
     if (received == total)
     error("ERROR storing complete response from socket");
 
-    /* close the socket/connection */
+    /* Closing the socket */
     close(sockfd);
     /* *Optionally print the response: */
-    /* printf("Response:\n%s\n",response); */
+    /*printf("Response:\n%s\n",response);*/
 }
