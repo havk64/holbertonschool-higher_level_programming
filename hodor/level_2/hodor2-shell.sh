b7bbf251ed9758a58307bb0fb172b0eb9b1f87bd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-01 16:57:15 -0300

    Adding solution to Hodor level 2 using Shell

---
+++ hodor/level_2/hodor2-shell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ===---Hodor Level 1-------------------------------------------------------------===
+# Solution in Shell Script for Hodor Project by Julien Barbier - Holberton School
+# Using curl command taking advantage of its default 'keep-alive' connection.
+# If you want to see more details about each request replace the "s" option with "v"
+# right after the 'curl' command.
+# ===-----------------------------------------------------------------------------===
+
+URL=http://173.246.108.142/level2.php
+ID='id=23'
+COOKIE='990aede99b2115451d612b8c50ae6332a8026b62'
+AGENT='Windows NT 5.0.0.0.1alpha - (Im kidding, I hate windows! Lol)'
+HEADER="HoldTheDoor=$COOKIE"
+COUNT=0
+VOTES=$((1 << 10)) #  = 1024
+
+### Voting 1024 times:
+while [ $COUNT -lt $VOTES ]
+do
+    curl -v -d "$ID&holdthedoor=submit&key=$COOKIE" -A "$AGENT" -b $HEADER --referer $URL $URL -o /dev/null
+    echo "Vote: "$COUNT
+    ((COUNT++))
+done
