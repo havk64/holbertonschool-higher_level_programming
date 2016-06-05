2eac641572405944e32827300dbc9c1a1f423ddd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-05 20:33:30 -0300

    Task 0 completed

---
+++ basic_sql_playground/0-main.sql
@@ -0,0 +1,10 @@
+/*
+ * ===---SQL Playground 0-------------------------------------------------------===
+ *  SQL Script for SQL Playground Project Level 1, by Guillaume
+ *
+ *  by Alexandro de Oliveira, for Holberton School.
+ * ===--------------------------------------------------------------------------===
+ */
+
+-- Displaying the schema of a SQL Table:
+.schema
