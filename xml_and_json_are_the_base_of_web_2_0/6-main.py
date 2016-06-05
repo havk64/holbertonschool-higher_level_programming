85e9a6edf145b536397fb50a56bf262f1d5eef50
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-05 18:51:23 -0300

    Adding comments

--- xml_and_json_are_the_base_of_web_2_0/6-main.py
+++ xml_and_json_are_the_base_of_web_2_0/6-main.py
@@ -1,14 +1,16 @@
 from json import loads
 from car import Car
 
+"""Opening the json file"""
 with open('./6-main.json') as f:
     data = loads(f.read())
 
+"""Creating the car objects:"""
 cars = []
-
 for car in data:
     obj = Car(car)
     cars.append(obj)
 
+"""Printing each object"""
 for car in cars:
     print car.to_comma()
