b6e834b3e4801065a61360fb29fecb7ce911822e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-05 21:44:59 -0300

    Fixing description

--- basic_sql_playground/0-main.sql
+++ basic_sql_playground/0-main.sql
@@ -1,10 +1,10 @@
 /*
  * ===---SQL Playground 0-------------------------------------------------------===
- *  SQL Script for SQL Playground Project Level 1, by Guillaume
+ *  SQL Script for SQL Playground Project, by Guillaume
  *
  *  by Alexandro de Oliveira, for Holberton School.
  * ===--------------------------------------------------------------------------===
  */
 
 -- Displaying the schema of a SQL Table:
-.schema
+.schema;
