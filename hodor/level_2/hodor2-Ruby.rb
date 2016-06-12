bae5f9083cd43eb3ee2feb367b76581bd8c1af10
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-12 04:31:20 -0300

    Getting a cookie in the first (HEAD) request

--- hodor/level_2/hodor2-Ruby.rb
+++ hodor/level_2/hodor2-Ruby.rb
@@ -14,8 +14,6 @@ require 'net/http'
 url = "http://173.246.108.142/level2.php"
 # Parsing the URL:
 uri = URI.parse(url)
-# Setting the cookie:
-cookie = "6b695713db9b371a855adf1e229264c681f2e48"
 # Setting the http request:
 http = Net::HTTP.new(uri.host, uri.port)
 # Setting the http request headers:
@@ -23,23 +21,29 @@ headers = {
   'User-Agent' => 'Windows NT 100 (aka: Blue Screen of Death!) havk64 - Ruby Requests',
   'Content-Type' => 'application/x-www-form-urlencoded',
   'Referer' => url,
-  'Cookie' => 'HoldTheDoor=' << cookie
 }
+head = http.head(uri.path, headers)
+cookie = head['Set-cookie'].split('; ')[0]
+headers['Cookie'] = cookie
 # Setting POST request parameters:
 params = URI.encode_www_form({
                                'id' => "23",
                                'holdthedoor' => "submit",
-                               'key' => cookie,
+                               'key' => cookie.split('=')[1],
                              })
 # Processing the POST requests:
 votes = 0 # A vote counter
-(1 << 10).times {  # The result of this bitwise operatation(1 << 10) is "1024"
-  resp, data = http.post(uri.path, params, headers)
-  unless resp.message != 'OK'
-    puts "Vote number: #{votes += 1}"
-    # Optionally printing the response object hash:
-    #puts "Headers: #{resp.to_hash.inspect}"
-  else
-    puts "Connection error, vote not computed..."
-  end
-}
+total_votes = (1 << 10)
+loop do  # The result of this bitwise operatation(1 << 10) is "1024"
+    resp, _ = http.post(uri.path, params, headers)
+    unless resp['Set-Cookie'].nil?
+        puts "Vote number: #{votes += 1}"
+        break if votes == total_votes
+        # Optionally printing the response object hash:
+        #puts "Headers: #{resp.to_hash.inspect}"
+    else
+        puts "Failed! (Vote not computed)"
+    end
+end
+
+puts "Total of valid votes is: #{votes}"
