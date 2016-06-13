538963a09320b77aba76658c482a7f92e4b00ac9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-13 03:36:07 -0300

    First commit Rlang solution to Hodor 3

---
+++ hodor/level_3/Rlang/hodor3-Rlang.r
@@ -0,0 +1,28 @@
+# ===---Description------------------------------------------------------------===
+#   Solution for the Hodor Project, Level 3 by Julien Barbier.
+#   Using a R Language script with httr library.
+#   To run this file install R language and run:
+#   $ R -f filename
+#
+#   by Alexandro de Oliveira, for Holberton School
+# ===---------------------------------------------------------------------------===
+
+library('httr')
+
+url     <- "http://173.246.108.142/level3.php"
+head    <- HEAD(url)            # Head request
+cookie  <- cookies(head)$value
+body    <- list(id = "23", holdthedoor = "submit", key = cookie)
+header  <- add_headers("User-Agent" = "Windows NT 5.1 - havk64 R Language Requests", "Referer" = url)
+
+for(i in 1:1024) {
+    # Some great code goes here!  >:-)
+    p <- POST(url, body = body, encode = "form", header)
+    s <- content(p, "text", encoding = 'UTF8')[1]
+    match = grep("HoldTheDoor", s, perl=TRUE, value=FALSE) # Regexp to check of vote is valid.
+    if(!is.null(match)) {
+        cat("Vote number: ", i , "\n")
+    } else {
+        cat( "Failed...", "\n")
+    }
+}
