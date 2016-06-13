9885f45d57e1bb014d3e6ce802a44e61c8b95005
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-13 03:32:09 -0300

    Fixing number of votes, ID

--- hodor/level_2/hodor2-Rlang.r
+++ hodor/level_2/hodor2-Rlang.r
@@ -12,10 +12,10 @@ library('httr')
 url     <- "http://173.246.108.142/level2.php"
 head    <- HEAD(url)            # Head request
 cookie  <- cookies(head)$value
-body    <- list(id = "1", holdthedoor = "submit", key = cookie)
+body    <- list(id = "23", holdthedoor = "submit", key = cookie)
 header  <- add_headers("User-Agent" = "Windows NT 5.1 - havk64 R Language Requests", "Referer" = url)
 
-for(i in 1:10) {
+for(i in 1:1024) {
     p <- POST(url, body = body, encode = "form", header)
     s <- content(p, "text", encoding = 'UTF8')[1]
     match = grep("I voted!", s, perl=TRUE, value=FALSE) # Regexp to check of vote is valid.
