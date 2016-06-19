58d859c974d89ce76e50c12e9075cfe80954da3e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-19 12:02:46 -0300

    Hodor 0 using Lisp/Elisp code

--- hodor/level_0/Lisp/hodor0.el
+++ hodor/level_0/Lisp/hodor0.el
@@ -1,3 +1,11 @@
+#!/usr/local/bin/emacs --script
+
+;; ===----Hodor Level 0-------------------------------------------------------------------------===
+;;      Voting from inside Emacs using `M-x ieml` or through *scratch*(Lisp Interaction) buffer.
+;;      Lisp is awesome!
+;;
+;;      by Alexandro de Oliveira, for Holberton School.
+;; ===------------------------------------------------------------------------------------------===
 (let ((url-request-method        "POST")
       (url-request-extra-headers `(("Content-Type" . "application/x-www-form-urlencoded")))
       (url-request-data          "id=23&holdthedoor=submit"))
