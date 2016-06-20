c06f7347b84861594791e11874c3b0bd0e44286c
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-20 01:21:19 -0300

    Hodor 2 solution using Erlang

--- hodor/level_2/Erlang/hodor2.erl
+++ hodor/level_2/Erlang/hodor2.erl
@@ -1,22 +1,23 @@
-%% ===-----Hodor Level 1---------------------------------------------===
-%%          Hodor Level 1 Project, by Julien Barbier.
-%%  "Make 1024 votes in this page: http://173.246.108.142/level1.php"
+%% ===-----Hodor Level 2---------------------------------------------===
+%%          Hodor Level 2 Project, by Julien Barbier.
+%%  "Make 1024 votes in this page: http://173.246.108.142/level2.php"
 %%  Solution using Erlang code.
 %%
 %%  by Alexandro de Oliveira.
 %% ===---------------------------------------------------------------===
 
--module(hodor1).
+-module(hodor2).
 -export([main/0, for/2, posting/0]).
 
 posting() ->
 Cok = "ba5ba0e6c4a60ec481444a36e5a39f07dbc1fe9b",
 Method = post,
-URL = "http://173.246.108.142/level1.php",
-Header = [{"User-Agent","havk64 Erlang Requests"},
-    {"Cookie", "HoldTheDoor=" ++ Cok}],
+URL = "http://173.246.108.142/level2.php",
+Header = [{"User-Agent","Windows NT 11111.0.0 havk64 Erlang Requests"},
+    {"Cookie", "HoldTheDoor=" ++ Cok},
+    {"Referer", URL}],
 Type = "application/x-www-form-urlencoded",
-Body = "id=23&holdthedoor=submit&key=" ++ Cok,
+Body = "id=404&holdthedoor=submit&key=" ++ Cok,
 ssl:start(),
 application:start(inets),
 %% Implementing cookie fetch and parse: (Not done yet)
