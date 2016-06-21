f621dfc09d09230e8b357d2a4f6a554732ae8d0b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-20 23:09:06 -0300

    Fixing request, improviments, more comments

--- hodor/level_2/Erlang/hodor2.erl
+++ hodor/level_2/Erlang/hodor2.erl
@@ -6,33 +6,41 @@
 %%  by Alexandro de Oliveira.
 %% ===---------------------------------------------------------------===
 
+%% Defining the module and exporting each function(to Erlang Shell):
 -module(hodor2).
--export([main/0, for/2, posting/0]).
-
+-export([main/0, vote/2, posting/0, parse/3, getCookie/1]).
+%% Parsing the Cookie(I didn't find a specific function/method to parse the cookie or
+%% a cookie session manager):
+parse(A, S, I) ->
+    T = string:tokens(A, S),
+    lists:nth(I, T). %% In Erlang the last line is returned(we don't need to "return").
+%% Getting one cookie with a HEAD request:
+getCookie(URL) ->
+    {ok,{ _,Headers, _}} = httpc:request(head, { URL, []}, [], []),
+    C = proplists:get_value("set-cookie", Headers),
+    Cookie = parse(C, ";", 1),
+    parse(Cookie, "=", 2).
+%% Making our POST request.
 posting() ->
-Cok = "ba5ba0e6c4a60ec481444a36e5a39f07dbc1fe9b",
 Method = post,
 URL = "http://173.246.108.142/level2.php",
-Header = [{"User-Agent","Windows NT 11111.0.0 havk64 Erlang Requests"},
-    {"Cookie", "HoldTheDoor=" ++ Cok},
+Cookie = getCookie(URL),
+Header = [{"User-Agent","Windows NT 5.1 havk64 Erlang Requests"}, {"Cookie", "HoldTheDoor=" ++ Cookie},
     {"Referer", URL}],
 Type = "application/x-www-form-urlencoded",
-Body = "id=404&holdthedoor=submit&key=" ++ Cok,
-ssl:start(),
-application:start(inets),
-%% Implementing cookie fetch and parse: (Not done yet)
-%{ok, _,Headers, _} = httpc:request(head, { URL, []}, [], []),
-%C = proplists:get_value("set-cookie", Headers),
-%A = string:tokens(C, ";"),
-%Cookie = array:get(0, A),
-httpc:request(Method, {URL, Header, Type, Body }, [], []).
+Body = "id=404&holdthedoor=submit&key=" ++ Cookie,
+httpc:request(Method, {URL, Header, Type, Body }, [], []). %% The POST request.
 
-for(0,_) ->
+%% Our "for loop" in a form of a recursive function(Erlang way of doing it)
+%% Functional(as opposed to Imperative) languages don't implement loops.
+vote(0,_) ->
    [];
-   for(N,Term) when N > 0 ->
+   vote(N,Term) when N > 0 ->
    posting(),
-   [Term|for(N-1,Term)].
+   [Term|vote(N-1,Term)].
 
 
 main() ->
-    for(1024, 1).
+    ssl:start(),
+    application:start(inets),
+    vote(104800, 1).
