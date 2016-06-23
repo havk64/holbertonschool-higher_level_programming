b38cab8f99307d7e6fadcf0eede87976665c8012
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-23 19:18:21 -0300

    Fixing comments

--- hodor/level_1/R/hodor1-Rlang.r
+++ hodor/level_1/R/hodor1-Rlang.r
@@ -8,7 +8,7 @@
 #   sessions:
 #   - Interactive -
 #   $ R
-#   $ source("nameoffile.r")
+#   > source("nameoffile.r")
 #   - Non Interactive:
 #   $ Rscript nameofthefile.r
 #
@@ -21,7 +21,6 @@ library('httr')
 # args	<- commandArgs(trailingOnly = TRUE)
 
 # Function to get a cookie from a HEAD request and set the header to next request.
-# (We could also to get just one cookie at the beginning and use it to all requests)
 setRequest  <- function() {
     head    <- HEAD(url)
     cookie  <- cookies(head)$value
@@ -34,7 +33,7 @@ setRequest  <- function() {
 # Defining variables:
 url     <- "http://173.246.108.142/level1.php"
 
-# Get number from user(interactive(R console) or non interactive(Rscript/batch) sessions):
+# Get number from user:
 cat("How many votes? => ")
 total   <- as.integer(readLines("stdin", 1))
 count   <- 0
