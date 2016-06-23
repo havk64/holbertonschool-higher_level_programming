eab68999558ccb4c886731cf172f1e43740d2bb6
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-22 22:44:52 -0300

    Improving control flow, including regexp

--- hodor/level_0/Erlang/hodor0.erl
+++ hodor/level_0/Erlang/hodor0.erl
@@ -1,31 +1,38 @@
 %% ===-----Hodor Level 0---------------------------------------------===
 %%          Hodor Level 0 Project, by Julien Barbier.
-%%  "Make 1024 votes in this page: http://173.246.108.142/level0.php"
+%%  "Vote 1024 in this page: http://173.246.108.142/level0.php"
 %%  Solution using Erlang code.
 %%
 %%  by Alexandro de Oliveira.
 %% ===---------------------------------------------------------------===
 
 -module(hodor0).
--export([main/0]).
+-export([main/0, vote/2, posting/1]).
 
-posting() ->
+posting(N) ->
 Method = post,
 URL = "http://173.246.108.142/level0.php",
 Header = [{"User-Agent","havk64 Erlang Requests"}],
 Type = "application/x-www-form-urlencoded",
 Body = "id=23&holdthedoor=submit",
-ssl:start(),
-application:start(inets),
-httpc:request(Method,
-    {URL, Header, Type, Body }, [], []).
+case httpc:request(Method,
+    {URL, Header, Type, Body }, [], []) of
+    {ok,{_,_,Body}} ->
+        case regexp:first_match(Header, ".*I voted.*") of
+            {match, _, _} -> io:format("Vote: ~s~n",[N]);
+             _            -> io:fwrite("failed.")
+        end;
+    {error, ErrorInfo} -> throw("Error: " ++ http:format_error(ErrorInfo))
+end.
 
-for(0,_) ->
+vote(0,_) ->
    [];
-   for(N,Term) when N > 0 ->
-   posting(),
-   [Term|for(N-1,Term)].
+   vote(N,Term) when N > 0 ->
+   posting(N),
+   [Term|vote(N-1,Term)].
 
 
 main() ->
-    for(1024, 1).
+    ssl:start(),
+    application:start(inets),
+    vote(1024, 1).
