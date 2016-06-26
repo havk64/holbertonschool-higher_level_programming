df9683a054e2ccea2641991882fefd20620fe4c9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-26 02:54:13 -0300

    Lua script to Hodor Level 1

---
+++ hodor/level_1/Lua/utils.lua
@@ -0,0 +1,40 @@
+
+--[=====[ (<<< Multiline comments in Lua)
+ ===-----------------------------------------------------------------------===
+      Defining a couple of helper functions in order to make the code
+      cleaner and more readable.
+
+      By Alexandro de Oliveira.
+ ===-----------------------------------------------------------------------===
+--]=====]
+
+-- Function to print a table:
+function p (s)
+    for k, v in pairs(s) do
+        print (k ,"=>", v)
+    end
+end
+
+-- Function split returns a Table(array like) of strings.
+-- Lua doesn't have a builtin "split" method, but it has regexp. Using it here:
+function split(str, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(str, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+-- Function to parse a cookie from the http header(that uses the split function):
+function parse(str)
+    local cookie = split(str, ";")[1]
+    local value = split(cookie, "=")[2]
+    return {
+        cookie = cookie,
+        value = value
+    }
+end
