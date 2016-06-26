49fcf50bace5f273d3a933c7f80ec108aa768cd3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-26 17:14:29 -0300

    Lua solution for Hodor level 3, working

--- hodor/level_3/Lua/hodor3.lua
+++ hodor/level_3/Lua/hodor3.lua
@@ -1,61 +1,69 @@
 #!/usr/bin/lua
 --[=====[
  ===-----------------------------------------------------------------------===
-      Hodor Project level 2, by Julien Barbier
-      Using Lua programming language.
+      Hodor Project level 3, by Julien Barbier
+      Using Lua programming language and Tesseract-OCR.
 
       By Alexandro de Oliveira.
  ===-----------------------------------------------------------------------===
 --]=====]
-
+function trim(s)
+ local from = s:match"^%s*()"
+ return from > #s and "" or s:match(".*%S", from)
+end
 --  To install the http package do: $ luarocks install luasocket
 local http = require "socket.http"
 local ltn12 = require "ltn12"
 local lfs = require "lfs"
+-- local io = require "io" --> Uncomment to use io.stdout to print the html body.
 require "utils" -- Importing the external file utils.lua.
 
-local url = "http://173.246.108.142/captcha.php"
+local url = "http://173.246.108.142/level3.php"
+local captcha_url = "http://173.246.108.142/captcha.php"
 local count = 0
 io.write("How many votes? => ") -- Prints the message requesting total votes.
---local total = io.read("*n")     -- Reading a number from user.
-
-local filePath = lfs.currentdir().."/tmp.png"
-print (filePath)
-local img = assert(io.open(filePath, "w"))
-local n, ok, header, line, e, f = http.request {
-    url = url,
-    sink = ltn12.sink.file(img)
-}
-img.close()
-print ( line, type(e), e, f, type(f))
-
---[=====[
-n = number (1)
-ok = number (200)
-header = table
-line = http 1.1 200 (string)
+local total = io.read("*n")     -- Reading a number from user.
 
 while count < total do
+    -- =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+    -- Fetch the captcha, decoding it, and parse PHP session cookie:
+    local filePath = lfs.currentdir().."/tmp.png"
+    local img = assert(io.open(filePath, "w"))
+    local n, ok, header = http.request {
+        url = captcha_url,
+        sink = ltn12.sink.file(img)
+    }
+    img.close()
+    local handle = io.popen("tesseract tmp.png - 2> /dev/null")
+    local captcha = trim(handle:read("*a")); handle.close()
+    --print (type(handle), '\"'..captcha..'\"', string.len(captcha))
+    --print (line, p(header))
+
+    local cookie_session = parse(header['set-cookie'])['cookie']
+    -- Check the PHP session cookie value:
+    -- print (cookie_session)
+
+    -- =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
     -- HEAD request to get a cookie and use it in the POST request.
     local body, code, headers, status = http.request {
         method = "HEAD",
         url = url,
         headers =
                 {
-                        ['User-Agent'] = "Windows NT 5.1 'Moonlight' Havk64 Lua Requests"
+                        ['User-Agent'] = "Windows NT 5.1 'Moonlight' Havk64 Lua Requests",
+                        ['Cookie'] = cookie_session -- Using the PHP session cookie.
                 }
     }
     local hc = headers['set-cookie']
-    -- print (hc)
     local tcookie = parse(hc)
     local cookie = tcookie['cookie']
     local value = tcookie['value']
     -- Print the Header of HEAD request:
     -- print (p(headers))
 
+    -- =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
     -- Preparing the POST request:
-    local reqbody = "id=23&holdthedoor=submit&key="..value
-    local respbody = {}
+    local reqbody = "id=23&holdthedoor=submit&key="..value.."&captcha="..captcha
     local body, code, headers, status = http.request {
         method = "POST",
         url = url,
@@ -67,13 +75,12 @@ while count < total do
                         ["Accept-Language"] = "en-us",
                         ["Referer"] = url,
                         ['User-Agent'] = "Windows NT 5.1 'Moonlight' Havk64 Lua Script Requests",
-                        ["Cookie"] = cookie,
+                        ["Cookie"] = cookie.."; "..cookie_session,
                         ["Content-Type"] = "application/x-www-form-urlencoded",
                         ["content-length"] = string.len(reqbody)
                     },
-
-                    sink = ltn12.sink.table(respbody)
-
+                -- Print the response body(html):
+                -- sink = ltn12.sink.file(io.stdout)
         }
         -- Checking the response header:
         -- print (p(headers))
@@ -85,4 +92,3 @@ while count < total do
             print("Failed")
         end
 end
---]=====]
