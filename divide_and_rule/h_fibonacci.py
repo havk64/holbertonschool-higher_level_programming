8dc68913cc624c8f07059914e320e0f036643a71
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-28 03:13:52 -0300

    Adding comments task 0

--- divide_and_rule/h_fibonacci.py
+++ divide_and_rule/h_fibonacci.py
@@ -1,13 +1,27 @@
+"""
+ ===----------------------------------------------------------===
+        Divide and Rule Project by Guillaume
+        Concurrency in Python.
+        Task 0: Fibonacci concurrently
+
+        by Alexandro de Oliveira for Holberton School.
+ ===----------------------------------------------------------===
+"""
+
+
 from threading import Thread
 
 class FibonacciThread(Thread):
+    """Defining fibonaccy class"""
     def __init__(self, number):
+        Thread.__init__(self)
         if not isinstance(number,int):
             raise Exception("number is not an integer")
         self.__number = number
-        Thread.__init__(self)
+
 
     def run(self):
+        """Action to be taken when the thread starts"""
         a, b = 1, 1
         for i in range(self.__number - 1):
             a, b = b, a + b
