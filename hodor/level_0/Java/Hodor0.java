6bc28cbce7bd4c00f9df97832b2ade2b2b37f8b4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-06-29 03:23:58 -0300

    Improving Java solution, option to print header

--- hodor/level_0/Java/Hodor0.java
+++ hodor/level_0/Java/Hodor0.java
@@ -8,15 +8,18 @@
  */
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.OutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class Hodor0 {
+	private static final String  P_URL = "http://173.246.108.142/level0.php";
 	private final String USER_AGENT = "Havk64 Java http requests";
 	public static void main(String[] args) throws Exception {
 		Hodor0 http = new Hodor0();
@@ -31,8 +34,7 @@ public class Hodor0 {
 	}
     // Function to set and send our POST request:
 	private void sendPost(int n) throws Exception {
-		String url = "http://173.246.108.142/level0.php";
-		URL obj = new URL(url);
+		URL obj = new URL(P_URL);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("POST");
 		con.setRequestProperty("User-Agent", USER_AGENT);
@@ -40,8 +42,14 @@ public class Hodor0 {
 		String urlParameters = "id=23&holdthedoor=submit";
 		// Send the params body:
 		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
+		OutputStream wr = con.getOutputStream();
+		// Parsing and optionally print the header:
+		/*
+		Map<String, List<String>> header = con.getHeaderFields();
+		for (Map.Entry<String, List<String>> entry: header.entrySet()) {
+			System.out.println(entry.getKey() + " => " + entry.getValue());
+		}*/
+		wr.write(urlParameters.getBytes());
 		wr.flush();
 		wr.close();
 		int responseCode = con.getResponseCode();
