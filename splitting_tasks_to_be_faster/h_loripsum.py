198758d517dc848345176105d8132ed96ecb5049
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-02 02:41:42 -0300

    Improving solution

--- splitting_tasks_to_be_faster/h_loripsum.py
+++ splitting_tasks_to_be_faster/h_loripsum.py
@@ -1,6 +1,6 @@
 from threading import Thread, Lock
 from requests import get
-import sys
+
 class LoripsumThread(Thread):
     """Definition of LoripsumThread"""
     def __init__(self, filename):
@@ -10,14 +10,12 @@ class LoripsumThread(Thread):
     def run(self):
         """Catching exceptions"""
         with Lock():
-            reload(sys)
-            sys.setdefaultencoding("latin-1")
             try:
                 res = get("http://loripsum.net/api/1/short")
             except:
                 raise Exception("GET request failed")
             try:
                 with open(self.__filename, 'a') as f:
-                    f.write(res.text)
+                    f.write(res.text.encode('utf-8'))
             except:
                 raise Exception("Could not open the file")
