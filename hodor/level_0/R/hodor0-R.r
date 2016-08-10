99186c9a23071043554df3d07e459473e96a1667
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-10 01:41:52 -0300

    Using 'integer' instead of 'numeric', total votes

--- hodor/level_0/R/hodor0-R.r
+++ hodor/level_0/R/hodor0-R.r
@@ -12,18 +12,18 @@ library('httr')
 url     <- "http://173.246.108.142/level0.php"
 header  <- add_headers("User-Agent" = "havk64 R Language Requests")
 body    <- list(id = "23", holdthedoor = "submit")
-count   <- 1
+count   <- 1L
 total   <- as.integer(readline(prompt = "How many votes? => "))
 
 while(count < total) {
     p <- POST(url, body = body, encode = "form", header)
     s <- content(p, "text", encoding = 'UTF8')[1]
-    match = grep("I voted!", s, perl=TRUE, value=FALSE) # Regexp to check of vote is valid.
+    match = grep("I voted!", s, perl = TRUE, value = FALSE) # Regexp to check of vote is valid.
     if(!is.null(match)) {
         cat('.')
-        count <- count + 1
+        count <- count + 1L
     } else {
         cat('x')
     }
 }
-cat('\n')
+cat("\n", "Total votes:", total, '\n')
