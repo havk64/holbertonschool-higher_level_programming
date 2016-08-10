237acdd2db3ed00b2f88a9ac6ba6e95c3fd68271
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-10 13:59:22 -0300

    Added more comments, amount from user input

--- hodor/level_2/R/hodor2-Rlang.r
+++ hodor/level_2/R/hodor2-Rlang.r
@@ -1,8 +1,14 @@
 # ===---Description------------------------------------------------------------===
 #   Solution for the Hodor Project, Level 2 by Julien Barbier.
 #   Using a R Language script with httr library.
-#   To run this file install R language and run:
-#   $ R -f filename
+#   To run this file you need to install R language.
+#   You can run it using interactive(R console) or non interactive(Rscript)
+#   sessions:
+#   - Interactive:
+#   $ R
+#   > source("nameoffile.r")
+#   - Non Interactive:
+#   $ Rscript nameofthefile.r
 #
 #   by Alexandro de Oliveira, for Holberton School
 # ===---------------------------------------------------------------------------===
@@ -11,18 +17,21 @@ library('httr')
 
 url     <- "http://173.246.108.142/level2.php"
 header  <- add_headers("User-Agent" = "Windows NT 5.1 - havk64 R Language Requests", "Referer" = url)
+count   <- 0
+# Get number from user:
+cat("How many votes? => ")
+total   <- as.integer(readLines("stdin", 1))
 
-for(i in 1:1024) {
+while(count < total) {
     head    <- HEAD(url)            # Head request
-    Sys.sleep(1)
     cookie  <- cookies(head)$value
     cat(cookie[2], "\n")
-    body    <- list(id = "1", holdthedoor = "submit", key = cookie[2])
-    p   <- POST(url, body = body, encode = "form", header)
+    body    <- list(id = "23", holdthedoor = "submit", key = cookie[2])
+    p       <- POST(url, body = body, encode = "form", header)
     # To print the body:
     # s <- content(p, "text", encoding = 'UTF8')[1]
     # cat(s, "\n")
-    match <- headers(p)$`set-cookie`
+    match    <- headers(p)$`set-cookie`
     if(!is.null(match)) {
         cat("Vote number: ", i , "\n")
     } else {
