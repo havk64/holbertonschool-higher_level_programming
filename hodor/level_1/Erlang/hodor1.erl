694208482a18b1a257947e5838814214ee2a056a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-27 15:39:43 -0300

    Head request to get first cookie, start inets just once

--- hodor/level_1/Erlang/hodor1.erl
+++ hodor/level_1/Erlang/hodor1.erl
@@ -9,22 +9,28 @@
 -module(hodor1).
 -export([main/0, for/2, posting/0]).
 
+% Parsing the Cookie(I didn't find a specific function/method to parse the cookie or
+%% a cookie session manager):
+parse(A, S, I) ->
+    T = string:tokens(A, S),
+    lists:nth(I, T). %% In Erlang the last line is returned(we don't need to "return").
+%% Getting one cookie with a HEAD request:
+getCookie(URL) ->
+    {ok,{ _,Headers, _}} = httpc:request(head, { URL, []}, [], []),
+    C = proplists:get_value("set-cookie", Headers),
+    Cookie = parse(C, ";", 1),
+    parse(Cookie, "=", 2).
+
 posting() ->
-Cok = "ba5ba0e6c4a60ec481444a36e5a39f07dbc1fe9b",
 Method = post,
 URL = "http://173.246.108.142/level1.php",
-Header = [{"User-Agent","havk64 Erlang Requests"},
-    {"Cookie", "HoldTheDoor=" ++ Cok}],
+Cookie = getCookie(URL),
+Header = [{"User-Agent","havk64 Erlang Requests"}, {"Cookie", "HoldTheDoor=" ++ Cookie}],
 Type = "application/x-www-form-urlencoded",
-Body = "id=23&holdthedoor=submit&key=" ++ Cok,
-ssl:start(),
-application:start(inets),
-%% Implementing cookie fetch and parse: (Not done yet)
-%{ok, _,Headers, _} = httpc:request(head, { URL, []}, [], []),
-%C = proplists:get_value("set-cookie", Headers),
-%A = string:tokens(C, ";"),
-%Cookie = array:get(0, A),
-httpc:request(Method, {URL, Header, Type, Body }, [], []).
+Body = "id=23&holdthedoor=submit&key=" ++ Cookie,
+
+httpc:request(Method, {URL, Header, Type, Body }, [], []),
+io:fwrite(".").
 
 for(0,_) ->
    [];
@@ -34,4 +40,6 @@ for(0,_) ->
 
 
 main() ->
+    ssl:start(),
+    application:start(inets),
     for(1024, 1).
